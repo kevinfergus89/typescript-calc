ce2a8db067933d57abff5387dd6850a297f5e6e9
Kevin Fergus <kevinfergus@Kevins-MacBook-Air.local>
2023-11-06 15:11:30 -0600

    Number click value event set

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
@@ -16,9 +17,32 @@ const btnValues = [
 
 
 const App = () => {
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
+  const numClickHandler: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    const value = (e.target as Element).innerHTML;
+
+    if (calc.num.toString().length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? 0
+            : calc.num % 1 === 0
+            ? calc.num + Number(value)
+            : calc.num + Number(value),
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+}
   return (
     <Wrapper>
-      <Screen value="0" />
+      <Screen value={calc.num ? calc.num.toString() : calc.res.toString()} />
       <ButtonBox>
       {
           btnValues.flat().map((btn, i) => {
--- src/Props.tsx
+++ src/Props.tsx
@@ -3,6 +3,7 @@ type Props = {
     value?: String;
     className?: string;
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    e?: Event;
     // any props that come into the component
   }
   
