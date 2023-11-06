9554374462a0f248c9e4b97ed49c9b60338f7e10
Kevin Fergus <kevinfergus@Kevins-MacBook-Air.local>
2023-11-06 14:47:13 -0600

    all buttons rendered

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
 
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  ["7", "8", "9", "X"],
+  ["4", "5", "6", "-"],
+  ["1", "2", "3", "+"],
+  ["0", ".", "="],
+];
 
 
 
@@ -13,13 +20,20 @@ const App = () => {
     <Wrapper>
       <Screen value="0" />
       <ButtonBox>
-        <Button
-          className=""
-          value="0"
-          onClick={() => {
-            console.log("Button clicked!");
-          }}
-        />
+      {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={() => {
+                  console.log(`${btn} clicked!`);
+                }}
+              />
+            );
+          })
+        }
       </ButtonBox>
     </Wrapper>
   );
--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -2,6 +2,13 @@ import "./Wrapper.css";
 import React from 'react';
 import "../Props"
 
+const btnValues = [
+    ["C", "+-", "%", "/"],
+    [7, 8, 9, "X"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ".", "="],
+  ];
   
 const Wrapper: React.FC<Props> = ({children }) => {
   return <div className="wrapper">{children}</div>;
